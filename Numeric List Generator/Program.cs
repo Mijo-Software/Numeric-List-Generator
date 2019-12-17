3ec018f14a99010c6619ef06aed91ea6caa7262e
Michael Johne <mjohne@users.noreply.github.com>
2019-12-17 22:42:59 +0100

    Add files via upload

--- Numeric List Generator/Program.cs
+++ Numeric List Generator/Program.cs
@@ -3,23 +3,23 @@ using System.Windows.Forms;
 
 namespace NumericListGenerator
 {
-  /// <summary>
-  /// Program
-  /// </summary>
-  internal static class Program
-  {
-    /// <summary>
-    /// main entrance point of the application
-    /// </summary>
-    [STAThread]
-    private static void Main()
-    {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(defaultValue: false);
-      using (NumericListGeneratorForm numericListGenerator = new NumericListGeneratorForm())
-      {
-        Application.Run(mainForm: numericListGenerator);
-      }
-    }
-  }
+	/// <summary>
+	/// Program
+	/// </summary>
+	internal static class Program
+	{
+		/// <summary>
+		/// main entrance point of the application
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+			using (NumericListGeneratorForm numericListGenerator = new NumericListGeneratorForm())
+			{
+				Application.Run(mainForm: numericListGenerator);
+			}
+		}
+	}
 }
